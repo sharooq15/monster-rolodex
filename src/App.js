7a9b70d3c1983684932d74ba404f8676e46090bf
sharooq15 <sharooqannauniv.ceg@gmail.com>
2021-04-17 13:19:38 +0530

    Fetch from a different source and display the content

--- src/App.js
+++ src/App.js
@@ -9,11 +9,16 @@ class App extends Component {
       { name: 'sharooq'}, { name: 'naveen'}, { name: 'jefree'}
     ]}
   }
+  async componentDidMount() {
+    const data = await (await fetch("https://jsonplaceholder.typicode.com/users")).json();
+    this.setState({monsters: data})
+  }
+
   render(){
     return (
       <div className="App">
         { 
-          this.state.monsters.map((monster) => <h1 key={monster.name}>
+          this.state.monsters.map((monster) => <h1 key={monster.id}>
             {monster.name}
           </h1>)
         }
