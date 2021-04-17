861c69b0da6a0cf1afbb67e284b26482ae7fd2aa
sharooq15 <sharooqannauniv.ceg@gmail.com>
2021-04-17 14:41:32 +0530

    Create components for cardlist and card

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import {
+  CardList
+} from '../src/components/card-list/card-list';
 import './App.css';
 
 class App extends Component {
@@ -17,11 +20,7 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        { 
-          this.state.monsters.map((monster) => <h1 key={monster.id}>
-            {monster.name}
-          </h1>)
-        }
+        <CardList monsters={this.state.monsters}/> 
       </div>
     );
   }
---
+++ src/components/card-list/card-list.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Card} from '../card/card'
+import './card-list.css'
+export const CardList = props => 
+<div className="card-list">{ 
+  props.monsters.map((monster) => <Card monster={monster}/>)
+}
+</div>
