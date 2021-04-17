9aceba0e2889a91f5f8e55f21c1698c1165b0495
sharooq15 <sharooqannauniv.ceg@gmail.com>
2021-04-17 13:03:27 +0530

    Learn iteration into state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      name: 'Sharooq'
-    } 
+      monsters: [
+      { name: 'sharooq'}, { name: 'naveen'}, { name: 'jefree'}
+    ]}
   }
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.name}</p>
-          <button onClick={() => this.setState({name: "s"})}>
-            change the name
-          </button>
-        </header>
+        { 
+          this.state.monsters.map((monster) => <h1 key={monster.name}>
+            {monster.name}
+          </h1>)
+        }
       </div>
     );
   }
